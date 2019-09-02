59ee844bbc3d463c056665c25992db191bbf2074
Patrick Kofod Mogensen <patrick.mogensen@gmail.com>
2019-09-02 22:32:10 +0200

    Initial commit.

--- src/MultiStart.jl
+++ src/MultiStart.jl
@@ -1,5 +1,114 @@
 module MultiStart
+using SpecialFunctions
+using LinearAlgebra
 
-greet() = print("Hello World!")
+export mlsl
+
+struct Sequential end
+struct Threading end
+function mlsl(obj, L, lower, upper; N, sigma, gamma, maxiter, getmin, getminf, partype=Sequential(), try_center=true)
+    ratio_kN = gamma
+
+    bounds = [(l, u) for (l, u) in zip(lower, upper)]
+    widths = [u-l for (l, u) in zip(lower, upper)]
+    VD = *(widths...)
+
+    T = eltype(lower)
+    n = length(lower)
+
+    if try_center
+        center = [l+w/2 for ((l, u), w) in zip(bounds, widths)]
+        obj_center = obj(center)
+        fXfrom = [obj_center]
+        Xfrom = [center]
+        X = [center]
+        fX = [obj_center]
+        lxr = L(obj, lower, upper, center)
+        Xstar = [getmin(lxr)]
+        fXstar = [getminf(lxr)]
+    else
+        # These and the accompanying methods allow for full inferrability of the
+        # arrays and of the code in the main loop with the methods defined below.
+        X = nothing
+        fX = nothing
+        Xfrom = nothing
+        fXfrom = nothing
+        Xstar = nothing
+        fXstar = nothing
+    end
+
+    for k = 1:maxiter
+        rk = rₖ(T, n, k, N, VD, sigma)
+        nbest = ratio_kN*k*N
+        nbest = ceil(Int, nbest) # since ratio_kN<1.0 we can safely round up
+        X, fX = reduced_set!(fX, X, partype, bounds, widths, obj, N)
+        Xr, fXr = X[1:nbest], fX[1:nbest]
+        for (xsearch, fxsearch) in zip(Xr, fXr)
+            # Skip if in a cluster
+            in_cluster(Xstar, fXstar, xsearch, fxsearch, rk) && continue
+
+            lxr = L(obj, lower, upper, xsearch)
+
+            Xfrom, fXfrom, Xstar, fXstar = update_arrays!(fXfrom, Xfrom, Xstar, fXstar,
+             xsearch, fxsearch, getmin(lxr), getminf(lxr))
+        end
+    end
+    Xstar, fXstar, Xfrom, fXfrom
+end
+
+function update_arrays!(fXfrom::Nothing, Xfrom::Nothing, Xstar::Nothing, fXstar::Nothing, xsearch, fxsearch, lxr_minx, lxr_min)
+    return [xsearch], [fxsearch], [lxr_minx], [lxr_min]
+end
+function update_arrays!(fXfrom, Xfrom, Xstar, fXstar, xsearch, fxsearch, lxr_minx, lxr_min)
+    push!(fXfrom, fxsearch)
+    push!(Xfrom, xsearch)
+    push!(Xstar, lxr_minx)
+    push!(fXstar, lxr_min)
+    return Xfrom, fXfrom, Xstar, fXstar
+end
+
+in_cluster(Xstar::Nothing, fXstar::Nothing, xsearch, fxsearch, rk) = false
+function in_cluster(Xstar, fXstar, xsearch, fxsearch, rk)
+    break_now = false
+    for (x, fx) in zip(Xstar, fXstar)
+        is_close = norm(xsearch-x) < rk
+        Δf = fxsearch - fx
+        is_larger = fxsearch >= fx
+        if is_close && is_larger
+            break_now = true
+        end
+        if break_now
+            return true
+        end
+    end
+    false
+end
+
+function reduced_set!(fX::Nothing, X::Nothing, partype, bounds, widths, obj, N)
+    # should do this in a sorted way
+    X = sample_N(bounds, N, widths)
+    fX = obj.(X)
+    p = sortperm(fX)
+    X .= X[p]
+    fX .= fX[p]
+    X, fX
+end
+function reduced_set!(fX, X, partype, bounds, widths, obj, N)
+    # should do this in a sorted way
+    Xnew = sample_N(bounds, N, widths)
+    push!(X, Xnew...)
+    fXnew = obj.(Xnew)
+    push!(fX, fXnew...)
+    p = sortperm(fX)
+    X .= X[p]
+    fX .= fX[p]
+    X, fX
+end
+function sample_N(bounds, N, widths)
+    [[rand()*width+b[1] for (b, width) in zip(bounds, widths)] for i = 1:N]
+end
+function rₖ(T, n, k, N, VD, sigma)
+    1/√T(π)*(gamma(T(1)+T(n)/2)*VD*(sigma*log(k*N)/k*N))^(1/T(n))
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,41 @@
-using MultiStart
+using MultiStart, Optim
 using Test
+function Hosaki(x)
+    T = eltype(x)
+    pol = Base.Math.@horner(x[1], T(1), -T(8), T(7), -T(7)/3, T(1)/4)
+
+    fx = exp(-x[2])*pol*x[2]^2
+end
+
+L = (f, lb, ub, x) -> optimize(f, lb, ub, x, Fminbox(BFGS()))
+
+hosaki_res = mlsl(Hosaki, L, [0.0, 0.0], [10.0, 10.0]; N=5, sigma=4.0, gamma=0.1, maxiter=100, getmin=Optim.minimizer, getminf=Optim.minimum)
+
+
+function HolderTable(x)
+    fact1 = sin(x[1])*cos(x[2])
+    fact2 = exp(abs(1-sqrt(x[1]^2+x[2]^2)/π))
+    -abs(fact1*fact2)
+end
+holder_res=mlsl(HolderTable, L, [-10.0, -10.0], [10.0, 10.0]; N=5, sigma=4.0, gamma=0.1, maxiter=100, getmin=Optim.minimizer, getminf=Optim.minimum)
+
+function Rastrigin(x)
+    T = eltype(x)
+    d = length(x)
+    10*d + sum(xᵢ^2-10*cos(2π*xᵢ) for xᵢ in x)::T
+end
+n_rastrigin = 3
+rastrigin_x, rastrigin_fx, from_x, from_fx=mlsl(Rastrigin, L, fill(-5.12, n_rastrigin), fill(5.12, n_rastrigin); N=5, sigma=4.0, gamma=0.1, maxiter=100, getmin=Optim.minimizer, getminf=Optim.minimum)
+
+using LinearAlgebra
+function Dropwave(x)
+    normx = norm(x)
+    num = 1 + cos(12*norm(x))
+    den = normx^2/2 + 2
+    -num/den
+end
+dropwave_x, dropwave_fx, from_x, from_fx=mlsl(Dropwave, L, fill(-5.12, 2), fill(5.12, 2); N=5, sigma=4.0, gamma=0.1, maxiter=100, getmin=Optim.minimizer, getminf=Optim.minimum, try_center=false)
 
 @testset "MultiStart.jl" begin
-    # Write your own tests here.
+
 end
